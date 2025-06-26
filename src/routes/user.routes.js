7aa388cb7c0f733bf5044c3b14b6bc8a00cdf8ca
MD IRFAN RAJ <mdirfanraj88.omega@gmail.com>
2025-06-26 20:05:15 +0530

    routes added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { loginUser, registerUser, logoutUser ,refreshAccessToken} from "../controllers/user.controller.js";
+import { loginUser, registerUser, logoutUser ,refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory} from "../controllers/user.controller.js";
 
 import {upload} from "../middlewares/multer.middleware.js"
 
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { verify } from "jsonwebtoken";
+import { verifyJWT } from './../middlewares/auth.middleware';
 
 
 
@@ -31,5 +33,24 @@ router.route("/logout").post(verifyJWT , logoutUser)
 //refresh token route
 router.route("/refresh-token").post(refreshAccessToken)
 
+
+//change pass
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+
+//current user
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+
+//upadte account details
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+
+
+//update avatar
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+
+router.route("/cover-image").patch(verifyJWT,upload.single("/coverImage"),updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+
+router.route("/watchhistory").get(verifyJWT,getWatchHistory)
 
 export default router
